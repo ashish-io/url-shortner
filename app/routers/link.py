1ade8e946f59885cb26c9b86186fd9e9d1f2021d
Ashish Parajuli <parajuliashish6@gmail.com>
2026-09-08 19:32:55 +0545

    Added redirection

--- app/routers/link.py
+++ app/routers/link.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from ..models.link import LinkCreate, Link
 from ..core.database import get_session
-from sqlmodel import Session
+from sqlmodel import Session, select
 from ..services.link_service import create_unique_short_link
+from fastapi.responses import RedirectResponse
+
+
 
 
 router = APIRouter()
@@ -15,6 +18,16 @@ def create_and_store_short_code(url: LinkCreate, session: Session = Depends(get_
 
 @router.get("/{short_code}")
 def redirect_to_long_url(short_code: str, session: Session = Depends(get_session)):
+  statement = select(Link).where(Link.short_code == short_code)
+  result = session.exec(statement).first()
+
+  if result is None:
+    raise HTTPException(status_code=404, detail="Link not found")
+  else:
+    return RedirectResponse(url=result.long_url, status_code=302)
+  
+
+
 
 
 
